acb496277611d561320a0438e9a866759b03c274
Anagha0718 <63951588+Anagha0718@users.noreply.github.com>
2020-11-26 08:11:00 +0530

    Update books.php

--- books.php
+++ books.php
@@ -2,14 +2,14 @@
 <html>
 <head><title>LIBRARY MANAGEMENT SYSTEM</title></head>
 <body>
-<form action = "" method ="post">
-<label>Name</label><input type ="text" name =""><br>
-<label>Boohnumbeer</label><input type ="text" name =""><br>
-<label>Title</label><input type ="text" name =""><br>
-<label>Authorname</label><input type ="text" name =""><br>
-<label>Edition</label><input type ="text" name =""><br>
-<label>Publication</label><input type ="text" name =""><br>
-<button type ="submit" value = "insert" name ="add">ADD</button>
+<form action = "books.php" method ="post">
+<label>NAME</label><input type ="text" name ="NAME"><br>
+<label>Booknumbeer</label><input type ="text" name ="Booknumber"><br>
+<label>Title</label><input type ="text" name ="Title">Title<br>
+<label>Authorname</label><input type ="text" name ="Authorname"><br>
+<label>Edition</label><input type ="text" name ="Edition"><br>
+<label>Publication</label><input type ="text" name ="Publication"><br>
+<button type ="submit" value = "insert" name ="submit">submit</button>
 </form>
 <?php
 $servername = "localhost";
@@ -19,8 +19,9 @@ $dbname = "book";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}if(isset($_POST['ADD']{
-$Name=$_POST['NAME'];
+}if(isset($_POST['submit'])){
+echo 'yes';
+$NAME=$_POST['NAME'];
 $Booknumbeer=$_POST['BOOK_NO'];
 $Title=$_POST['TITLE'];
 $Authorname=$_POST['AUTHOR NAME'];
@@ -28,7 +29,7 @@ $Edition=$_POST['EDITION'];
 $Publication=$_POST['PUBLICATION'];
 }
 
-$sql = "INSERT INTO details(NAME,BOOK_NO,TITLE,AUTHOR NAME,EDITION,PUBLICATION) VALUES ('$Name','$Booknumbeer','$Title','$Authorname','$Edition','$Publication')";
+$sql = "INSERT INTO details(NAME,BOOK_NO,TITLE,AUTHOR NAME,EDITION,PUBLICATION) VALUES ("$Name","$Booknumbeer","$Title","$Authorname","$Edition","$Publication")";
 
 $run =mysqli_query($conn,$sql) or die(mysqli_error());
 if($run){
